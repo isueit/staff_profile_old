b46a9373fade80740dfe0301b3f9b99ff39f8e54
Tyler Gerwig <eit_tcgerwig@iastate.edu>
2019-06-19 17:06:10 -0500

    Added field permissions and json profile feed

--- src/StaffProfileAccessControlHandler.php
+++ src/StaffProfileAccessControlHandler.php
@@ -49,18 +49,18 @@ class StaffProfileAccessControlHandler extends EntityAccessControlHandler {
     * {@inheritdoc}
     */
   protected function checkFieldAccess($operation, $field_definition, $account, $items = NULL) {
-    $protected_fields = ['field_program_area_s_', 'field_last_name', 'field_email', 'field_address_1', 'field_address_2', 'field_city', 'field_state', 'field_zip', 'field_phone', 'field_extension_region', 'field_location', 'field_department_id', 'field_base_county', 'field_counties_served'];
+    $protected_fields = ['field_program_area_s_', 'field_last_name', 'field_email', 'field_address_1', 'field_address_2', 'field_city', 'field_state', 'field_zip', 'field_phone', 'field_extension_region', 'field_location', 'field_department_id', 'field_base_county', 'field_counties_served', 'field_profile_image'];
      switch ($operation) {
        case 'edit':
-       debug(serialize($field_definition));
-       debug(serialize($items));
-        $access = AccessResult::allowedIfHasPermission($account, 'edit staff profile entity');
-    //     if ($account->hasPermission('administer staff profile entity') {}
+       $field = $field_definition->getName();
+        if (in_array($field, $protected_fields) && !$account->hasPermission('administer staff profile entity')) {
+          return AccessResult::forbidden();
+        }
          break;
        case 'view':
         return AccessResult::allowedIfHasPermission($account, 'access content');
         break;
      }
-    return AccessResult::allowed();
+    return AccessResult::allowedIfHasPermission($account, 'access content');
   }
 }
