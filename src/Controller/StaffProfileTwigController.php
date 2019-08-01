7ba0e6e4ee4b3435c7eac0850cc9f10924b1da7d
Tyler Gerwig <eit_tcgerwig@iastate.edu>
2019-08-01 17:10:09 -0500

    Removing duplicate images and theming search cards

--- src/Controller/StaffProfileTwigController.php
+++ src/Controller/StaffProfileTwigController.php
@@ -4,22 +4,22 @@
  * Contains \Drupal\staff_profile\Controller\StaffProfileTwigController.
  */
 
-namespace Drupal\staff_profile\Controller;
-
-use Drupal\Core\Controller\ControllerBase;
-
-class StaffProfileTwigController extends ControllerBase {
-  public function content() {
-    return [
-      '#theme' => 'search_result__staff_profile_profile_search',
-      '#url' => "",
-      '#title' => "",
-      '#snippet' => "",
-      '#info' => "",
-      '#plugin_id' => 'staff_profile_profile_search',
-      '#title_prefix' => "",
-      '#title_suffix' => "",
-      '#info_split' => [],
-    ];
-  }
-}
+// namespace Drupal\staff_profile\Controller;
+//
+// use Drupal\Core\Controller\ControllerBase;
+//
+// class StaffProfileTwigController extends ControllerBase {
+//   public function content() {
+//     return [
+//       '#theme' => 'search_result__staff_profile_profile_search',
+//       '#url' => "",
+//       '#title' => "",
+//       '#snippet' => "",
+//       '#info' => "",
+//       '#plugin_id' => 'staff_profile_profile_search',
+//       '#title_prefix' => "",
+//       '#title_suffix' => "",
+//       '#info_split' => [],
+//     ];
+//   }
+// }
